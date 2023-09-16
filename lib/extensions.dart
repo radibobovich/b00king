79d7d4af8adf5698509a2395ad1f40c8a4901e2b
radibobovich <radeonovich1@yandex.ru>
2023-09-16 17:55:56 +0300

    simplicity

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -2,7 +2,7 @@ import 'package:booking/theme.dart';
 import 'package:flutter/material.dart';
 
 extension BuildContextExt on BuildContext {
-  CustomColors get color => Theme.of(this).extension<CustomColors>()!;
+  CustomColors get color => AppTheme.appColors;
   ThemeData get theme => Theme.of(this);
   double get adaptiveWidth => MediaQuery.of(this).size.width;
 }
