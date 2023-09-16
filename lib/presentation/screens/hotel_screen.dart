9366bb53d70f40028afbc522ef1fec4ccfb65a7f
radibobovich <radeonovich1@yandex.ru>
2023-09-16 23:18:24 +0300

    precache in carousel

--- lib/presentation/screens/hotel_screen.dart
+++ lib/presentation/screens/hotel_screen.dart
@@ -1,51 +1,30 @@
 import 'package:booking/extensions.dart';
 import 'package:booking/presentation/cubit/hotel_cubit.dart';
+import 'package:booking/presentation/fonts.dart';
 import 'package:booking/presentation/widgets/error_builder.dart';
 import 'package:booking/presentation/widgets/shared/app_bar.dart';
 import 'package:booking/presentation/widgets/hotel/hotel_bottom_block.dart';
 import 'package:booking/presentation/widgets/hotel/hotel_top_block.dart';
+import 'package:booking/utils/image_preloader.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:core';
 
-class HotelScreen extends StatefulWidget {
+class HotelScreen extends StatelessWidget {
   const HotelScreen({super.key});
-
-  @override
-  State<HotelScreen> createState() => _HotelScreenState();
-}
-
-class _HotelScreenState extends State<HotelScreen> {
-  /// Pre-cache images so bloc will emit state only after images are loaded
-  /// This will prolong loading time, but also prevent flickering
-  @override
-  void didChangeDependencies() {
-    final cubit = context.read<HotelCubit>();
-    cubit.getHotel().then((hotel) async {
-      if (hotel != null) {
-        await precacheGallery(hotel.imageUrls, context);
-      }
-      return hotel;
-    }).then((value) {
-      if (value != null) {
-        cubit.displayHotel(value);
-      }
-    });
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const HotelAppBar(title: 'Отель'),
+        appBar: const SharedAppBar(title: 'Отель'),
         bottomSheet: const BottomButtonSheet(),
-        body: SingleChildScrollView(
-          child: BlocBuilder<HotelCubit, HotelState>(
-            builder: (context, state) {
-              if (state is HotelLoaded) {
-                final hotel = state.hotel;
-                return Column(
+        body: BlocBuilder<HotelCubit, HotelState>(
+          builder: (context, state) {
+            if (state is HotelLoaded) {
+              final hotel = state.hotel;
+              return SingleChildScrollView(
+                child: Column(
                   children: [
                     /// White block with gallery and main info
                     TopBlock(hotel: hotel),
@@ -53,14 +32,14 @@ class _HotelScreenState extends State<HotelScreen> {
                     BottomBlock(hotel: hotel),
                     const SizedBox(height: 82),
                   ],
-                );
-              } else if (state is HotelError) {
-                return ErrorPlaceholder(exception: state.exception);
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
+                ),
+              );
+            } else if (state is HotelError) {
+              return ErrorPlaceholder(exception: state.exception);
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ));
   }
 }
@@ -84,13 +63,15 @@ class BottomButtonSheet extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.fromLTRB(16, 14, 16, 9),
               child: CupertinoButton(
-                  borderRadius: BorderRadius.circular(15),
-                  color: context.color.addressColor,
-                  child: const Text(
-                    'К выбору номера',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  onPressed: () {}),
+                borderRadius: BorderRadius.circular(15),
+                color: context.color.addressColor,
+                child: Text(
+                  'К выбору номера',
+                  style: AppFonts.buttonLabel,
+                ),
+                onPressed: () => Navigator.pushNamed(context, '/rooms',
+                    arguments: state.hotel.name),
+              ),
             ),
           ),
           const Divider(
