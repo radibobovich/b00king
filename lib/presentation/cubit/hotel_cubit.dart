f7caa4058ce13748d36d0c0e6f2699224c018a46
radibobovich <radeonovich1@yandex.ru>
2023-09-16 23:15:50 +0300

    precache in background

--- lib/presentation/cubit/hotel_cubit.dart
+++ lib/presentation/cubit/hotel_cubit.dart
@@ -1,31 +1,18 @@
 import 'package:booking/domain/models/hotel.dart';
 import 'package:booking/domain/models/repositories/hotel_repository.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HotelCubit extends Cubit<HotelState> {
   final HotelRepository hotelRepository;
   HotelCubit({required this.hotelRepository}) : super(HotelInitial());
 
-  Future<Hotel?> getHotel() async {
+  Future<void> getHotel() async {
     try {
       final Hotel hotel = await hotelRepository.getHotel();
-      return hotel;
+      emit(HotelLoaded(hotel));
     } catch (e) {
       emit(HotelError(e as Exception));
-      return null;
     }
-  }
-
-  Future<void> displayHotel(Hotel hotel) async {
-    emit(HotelLoaded(hotel));
-  }
-}
-
-Future<void> precacheGallery(
-    List<String> imageUrls, BuildContext context) async {
-  for (var img in imageUrls) {
-    await precacheImage(NetworkImage(img), context);
   }
 }
 
