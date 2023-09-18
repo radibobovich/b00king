ace9428cc4b246aa7b365891d9e19b8d45cd9e57
radibobovich <radeonovich1@yandex.ru>
2023-09-18 16:38:23 +0300

    extract and refactor

--- lib/presentation/screens/booking_screen.dart
+++ lib/presentation/screens/booking_screen.dart
@@ -1,102 +1,155 @@
-import 'package:booking/domain/models/booking.dart';
 import 'package:booking/extensions.dart';
 import 'package:booking/presentation/cubit/booking_cubit.dart';
 import 'package:booking/presentation/fonts.dart';
+import 'package:booking/presentation/widgets/booking/booking_add_tourist_block.dart';
+import 'package:booking/presentation/widgets/booking/booking_customer_block.dart';
+import 'package:booking/presentation/widgets/booking/booking_tables.dart';
+import 'package:booking/presentation/widgets/booking/booking_tourist_block.dart';
 import 'package:booking/presentation/widgets/error_builder.dart';
 import 'package:booking/presentation/widgets/shared/app_bar.dart';
+import 'package:booking/presentation/widgets/shared/bottom_button_sheet.dart';
 import 'package:booking/presentation/widgets/shared/main_hotel_info.dart';
-import 'package:booking/utils/declension.dart';
+import 'package:booking/utils/price_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookingScreen extends StatelessWidget {
-  const BookingScreen({super.key});
-
+  const BookingScreen(
+      {super.key,
+      required this.touristsFormKey,
+      required this.phoneFormKey,
+      required this.emailFormKey,
+      required this.scrollController});
+  final GlobalKey<FormState> touristsFormKey;
+  final ScrollController scrollController;
+  final GlobalKey<FormState> phoneFormKey;
+  final GlobalKey<FormState> emailFormKey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SharedAppBar(title: 'Бронирование'),
-      body: BlocBuilder<BookingCubit, BookingState>(
-        builder: (context, state) {
-          if (state is BookingLoaded) {
-            final booking = state.booking;
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.fromLTRB(17, 0, 17, 19),
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    decoration: BoxDecoration(
-                        color: context.color.blockBackgroundColor,
-                        borderRadius: BorderRadius.circular(15)),
-                    child: MainHotelInfo(
-                        rating: booking.horating,
-                        ratingName: booking.ratingName,
-                        address: booking.hotelAddress),
-                  ),
-                  BookingInfoTable(booking: booking)
-                ],
-              ),
-            );
-          } else if (state is BookingError) {
-            return ErrorPlaceholder(exception: state.exception);
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      appBar: const SharedAppBar(title: 'Бронирование'),
+      bottomSheet: BookingBottomSheet(
+          phoneFormKey: phoneFormKey,
+          emailFormKey: emailFormKey,
+          touristsFormKey: touristsFormKey),
+      body: BookingBody(
+          scrollController: scrollController,
+          phoneFormKey: phoneFormKey,
+          emailFormKey: emailFormKey,
+          touristsFormKey: touristsFormKey),
     );
   }
 }
 
-class BookingInfoTable extends StatelessWidget {
-  const BookingInfoTable({super.key, required this.booking});
-  final Booking booking;
+class BookingBody extends StatelessWidget {
+  const BookingBody({
+    super.key,
+    required this.scrollController,
+    required this.phoneFormKey,
+    required this.emailFormKey,
+    required this.touristsFormKey,
+  });
+
+  final ScrollController scrollController;
+  final GlobalKey<FormState> phoneFormKey;
+  final GlobalKey<FormState> emailFormKey;
+  final GlobalKey<FormState> touristsFormKey;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.fromLTRB(17, 16, 17, 19),
-        decoration: BoxDecoration(
-            color: context.color.blockBackgroundColor,
-            borderRadius: BorderRadius.circular(15)),
-        child: Table(
-          columnWidths: const {
-            0: FractionColumnWidth(0.4),
-            1: FractionColumnWidth(0.6)
-          },
-          children: [
-            // TODO: replace with actual data
-            getTableRow('Вылет из', 'Санкт-Петербург'),
-            getTableRow('Страна, город', booking.arrivalCountry),
-            getTableRow(
-                'Даты', '${booking.tourDateStart} – ${booking.tourDateStop}'),
-            getTableRow('Кол-во ночей',
-                '${booking.numberOfNights} ${nightsDeclension(booking.numberOfNights)}'),
-            // TODO: replace with actual data
-            getTableRow('Отель', 'SteigenBerger Makadi'),
-            // TODO: replace with actual data
-            getTableRow('Номер', 'Стандартный с видом на бассейн или сад'),
-            getTableRow('Питание', booking.nutrition, isLast: true),
-          ],
-        ));
+    return BlocBuilder<BookingCubit, BookingState>(
+      builder: (context, state) {
+        if (state is BookingLoaded) {
+          final booking = state.booking;
+          final tourists = state.touristsInfo;
+          return SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(17, 0, 17, 19),
+                  margin: const EdgeInsets.only(top: 10, bottom: 8),
+                  decoration: BoxDecoration(
+                      color: context.color.blockBackgroundColor,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: MainHotelInfo(
+                      rating: booking.horating,
+                      ratingName: booking.ratingName,
+                      address: booking.hotelAddress),
+                ),
+                BookingInfoTable(booking: booking),
+                CustomerBlock(
+                  phoneFormKey: phoneFormKey,
+                  emailFormKey: emailFormKey,
+                ),
+                Form(
+                    key: touristsFormKey,
+                    child: Column(
+                      children: List.generate(
+                        tourists.length,
+                        (index) => TouristBlock(
+                            touristInfo: tourists[index], id: index + 1),
+                      ),
+                    )),
+                AddTouristBlock(scrollController: scrollController),
+                PricingTable(booking: booking),
+                const SizedBox(height: 82),
+              ],
+            ),
+          );
+        } else if (state is BookingError) {
+          return ErrorPlaceholder(exception: state.exception);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
+}
 
-  TableRow getTableRow(String infoName, String infoValue,
-      {bool isLast = false}) {
-    final padding =
-        isLast ? EdgeInsets.zero : const EdgeInsets.only(bottom: 18.25);
-    return TableRow(children: [
-      Padding(
-        padding: padding,
-        child: Text(
-          infoName,
-          style: AppFonts.bookingInfoName,
-        ),
-      ),
-      Padding(
-        padding: padding,
-        child: Text(infoValue, style: AppFonts.bookingInfoValue),
-      ),
-    ]);
+class BookingBottomSheet extends StatelessWidget {
+  const BookingBottomSheet({
+    super.key,
+    required this.phoneFormKey,
+    required this.emailFormKey,
+    required this.touristsFormKey,
+  });
+
+  final GlobalKey<FormState> phoneFormKey;
+  final GlobalKey<FormState> emailFormKey;
+  final GlobalKey<FormState> touristsFormKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BookingCubit, BookingState>(
+      builder: (context, state) {
+        if (state is BookingLoaded) {
+          final totalPrice = state.booking.tourPrice +
+              state.booking.fuelCharge +
+              state.booking.serviceCharge;
+          return BottomButtonSheet(
+            buttonText: 'Оплатить ${formatPrice(totalPrice)} ₽',
+            onPressed: () {
+              if (context.read<BookingCubit>().validateForms(
+                  [phoneFormKey, emailFormKey, touristsFormKey])) {
+                Navigator.of(context).pushNamed('/paid');
+              } else {
+                // snack bar
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  duration: const Duration(seconds: 2),
+                  content: Text(
+                    'Заполнены не все поля',
+                    style: AppFonts.errorSnackbarLabel,
+                  ),
+                  backgroundColor: context.theme.scaffoldBackgroundColor,
+                ));
+              }
+            },
+          );
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
+    );
   }
 }
