55833aa2d0f8c75e6e4ca1b94b6d0140e2b8bb5e
radibobovich <radeonovich1@yandex.ru>
2023-09-18 16:23:24 +0300

    add tourists management

--- lib/presentation/cubit/booking_cubit.dart
+++ lib/presentation/cubit/booking_cubit.dart
@@ -1,19 +1,36 @@
 import 'package:booking/domain/models/booking.dart';
+import 'package:booking/domain/models/booking_form.dart';
 import 'package:booking/domain/models/repositories/booking_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookingCubit extends Cubit<BookingState> {
-  BookingCubit({required this.bookingRepository}) : super(BookingInitial());
+  BookingCubit(
+      {required BookingRepository bookingRepository,
+      required this.bookingFormManager})
+      : _bookingRepository = bookingRepository,
+        super(BookingInitial());
 
-  final BookingRepository bookingRepository;
-
+  final BookingRepository _bookingRepository;
+  final BookingFormManager bookingFormManager;
+  late Booking _booking;
   void getBooking() async {
     try {
-      final booking = await bookingRepository.getBooking();
-      emit(BookingLoaded(booking: booking));
+      _booking = await _bookingRepository.getBooking();
+      emit(BookingLoaded(
+          booking: _booking, touristsInfo: bookingFormManager.touristsInfo));
     } catch (e) {
       emit(BookingError(e as Exception));
     }
+  }
+
+  void addNewTourist() async {
+    bookingFormManager.addTouristInfo(TouristInfo());
+    emit(BookingLoaded(
+        booking: _booking, touristsInfo: bookingFormManager.touristsInfo));
+  }
+
+  bool validateTouristsEmptyFields() {
+    return bookingFormManager.validateTouristsEmptyFields();
   }
 }
 
@@ -23,8 +40,8 @@ class BookingInitial extends BookingState {}
 
 class BookingLoaded extends BookingState {
   final Booking booking;
-
-  BookingLoaded({required this.booking});
+  final List<TouristInfo> touristsInfo;
+  BookingLoaded({required this.booking, required this.touristsInfo});
 }
 
 class BookingError extends BookingState {
